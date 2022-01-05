da7cc34126b6d445a71ecd074dedd926104d32c4
Nichele Dunn <the7yearitch@hotmail.com>
2022-01-05 15:53:00 -0700

    ✨ Add outline and pseudocode for Gallery component

---
+++ src/Gallery.js
@@ -0,0 +1,13 @@
+// Gallery component
+// Renders an artwork based on the hue selected by user
+
+import React from "react";
+
+function Gallery(props) {
+  console.log("Gallery props <>>>", props);
+
+  // In the first iteration, this component will accept props from the App, and be rendered conditionally
+  // When a user clicks on a ColorCard, a condition will update and this component will replace the Spectrum in the dashboard view
+}
+
+export default Gallery;
