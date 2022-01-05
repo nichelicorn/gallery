7445803f9e5fa5acf1152f6c5f5064153dc3b52b
Nichele Dunn <the7yearitch@hotmail.com>
2022-01-05 15:21:39 -0700

    ♻️ Convert Spectrum to a Class component

--- src/Spectrum.js
+++ src/Spectrum.js
@@ -1,16 +1,21 @@
 // Spectrum component
 // Renders rainbow-based art gallery in a flex-wrapper
 
+import React from "react";
+
 // should accept props from App, with color artwork assigned to an array
-function Spectrum() {
+class Spectrum extends React.Component {
 
   // map through spectrum array to create color artwork bubbles 
 
-  return (
-    <section className="spectrum-wrap">
-      {colorArtwork}
-    </section>
-  );
+  render() {
+    return (
+      <section className="spectrum-wrap">
+        {colorArtwork}
+      </section>
+    );
+  }
+
 }
 
 export default Spectrum;
