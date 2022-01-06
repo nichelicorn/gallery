6ab4c19f77b1e044134305d551ee4a2d3b33aae5
Nichele Dunn <the7yearitch@hotmail.com>
2022-01-06 16:16:17 -0700

    🧪 Create test data file and add three image objects from the API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Spectrum from "./Spectrum";
 import './App.css';
+import testImages from './test-data';
 
 class App extends React.Component {
-
+  constructor() {
+    super();
+    this.state = {
+      images: []
+    }
+  }
   // App state should contain { spectrum: [ r o y g b i v ] }
   // pass each color value on to the Spectrum
   // add fetch request to API for color-based images (read through ElasticSearch documentation in the API docs to set up queries)
@@ -15,11 +21,11 @@ class App extends React.Component {
   render() {
     return (
       <main className="App">
+        {/* add Header component here */}
         <header className="App-header">
           <h1>Spectrum</h1>
         </header>
-        {/* add Header component here */}
-        <Spectrum ></Spectrum>
+        <Spectrum></Spectrum>
         {/* when a user clicks on a ColorCard, render the Gallery view */}
         {/* add Footer component here */}
       </main>
---
+++ src/test-data.js
@@ -0,0 +1,202 @@
+const testImages = [
+  {
+    "date": null,
+    "copyright": "President and Fellows of Harvard College",
+    "imageid": 29728,
+    "idsid": 20678311,
+    "accesslevel": 1,
+    "format": "image/jpeg",
+    "caption": null,
+    "description": null,
+    "technique": "Ektachrome Professional",
+    "renditionnumber": "38681",
+    "colors": null,
+    "baseimageurl": "https://nrs.harvard.edu/urn-3:HUAM:VRS38681_dynmc",
+    "alttext": null,
+    "width": 723,
+    "id": 29728,
+    "lastupdate": "2022-01-06T05:06:56-0500",
+    "iiifbaseuri": "https://ids.lib.harvard.edu/ids/iiif/20678311",
+    "fileid": 4812161,
+    "height": 1024
+  },
+  {
+    "date": null,
+    "copyright": "President and Fellows of Harvard College",
+    "imageid": 29735,
+    "idsid": 20678341,
+    "accesslevel": 1,
+    "format": "image/jpeg",
+    "caption": null,
+    "description": null,
+    "technique": "Ektachrome Professional",
+    "renditionnumber": "38711",
+    "colors": [
+      {
+        "color": "#964b32",
+        "spectrum": "#d0577c",
+        "hue": "Orange",
+        "percent": "0.315887850467290",
+        "css3": "#a0522d"
+      },
+      {
+        "color": "#000000",
+        "spectrum": "#1eb264",
+        "hue": "Black",
+        "percent": "0.187165109034268",
+        "css3": "#000000"
+      },
+      {
+        "color": "#641919",
+        "spectrum": "#b65590",
+        "hue": "Red",
+        "percent": "0.120685358255452",
+        "css3": "#800000"
+      },
+      {
+        "color": "#af644b",
+        "spectrum": "#e0626d",
+        "hue": "Orange",
+        "percent": "0.117632398753894",
+        "css3": "#cd5c5c"
+      },
+      {
+        "color": "#190000",
+        "spectrum": "#2eb45d",
+        "hue": "Black",
+        "percent": "0.100996884735202",
+        "css3": "#000000"
+      },
+      {
+        "color": "#c87d64",
+        "spectrum": "#e66c64",
+        "hue": "Orange",
+        "percent": "0.064112149532710",
+        "css3": "#cd5c5c"
+      },
+      {
+        "color": "#190019",
+        "spectrum": "#1eb264",
+        "hue": "Brown",
+        "percent": "0.056947040498442",
+        "css3": "#000000"
+      },
+      {
+        "color": "#e1967d",
+        "spectrum": "#e9715f",
+        "hue": "Orange",
+        "percent": "0.018130841121495",
+        "css3": "#e9967a"
+      },
+      {
+        "color": "#e1af96",
+        "spectrum": "#e9715f",
+        "hue": "Orange",
+        "percent": "0.012024922118380",
+        "css3": "#deb887"
+      },
+      {
+        "color": "#fafafa",
+        "spectrum": "#955ba5",
+        "hue": "White",
+        "percent": "0.000872274143302",
+        "css3": "#fffafa"
+      }
+    ],
+    "baseimageurl": "https://nrs.harvard.edu/urn-3:HUAM:VRS38711_dynmc",
+    "alttext": null,
+    "width": 736,
+    "id": 29735,
+    "lastupdate": "2022-01-06T05:06:56-0500",
+    "iiifbaseuri": "https://ids.lib.harvard.edu/ids/iiif/20678341",
+    "fileid": 4812191,
+    "height": 1024
+  },
+  {
+    "date": "2002-12-18",
+    "copyright": "President and Fellows of Harvard College",
+    "imageid": 29748,
+    "idsid": 18211255,
+    "accesslevel": 1,
+    "format": "image/jpeg",
+    "caption": null,
+    "description": null,
+    "technique": null,
+    "renditionnumber": "CARP00896",
+    "colors": [
+      {
+        "color": "#af7d4b",
+        "spectrum": "#e9715f",
+        "hue": "Yellow",
+        "percent": "0.315135135135135",
+        "css3": "#cd853f"
+      },
+      {
+        "color": "#c89664",
+        "spectrum": "#ed7758",
+        "hue": "Brown",
+        "percent": "0.287297297297297",
+        "css3": "#bdb76b"
+      },
+      {
+        "color": "#96644b",
+        "spectrum": "#c25687",
+        "hue": "Brown",
+        "percent": "0.153603603603604",
+        "css3": "#a0522d"
+      },
+      {
+        "color": "#7d6432",
+        "spectrum": "#6cbd45",
+        "hue": "Yellow",
+        "percent": "0.125945945945946",
+        "css3": "#a0522d"
+      },
+      {
+        "color": "#644b32",
+        "spectrum": "#59ba4a",
+        "hue": "Brown",
+        "percent": "0.040360360360360",
+        "css3": "#556b2f"
+      },
+      {
+        "color": "#c8af7d",
+        "spectrum": "#e9715f",
+        "hue": "Brown",
+        "percent": "0.037747747747748",
+        "css3": "#d2b48c"
+      },
+      {
+        "color": "#e1e1c8",
+        "spectrum": "#e9715f",
+        "hue": "Green",
+        "percent": "0.022972972972973",
+        "css3": "#dcdcdc"
+      },
+      {
+        "color": "#4b3219",
+        "spectrum": "#4ab851",
+        "hue": "Brown",
+        "percent": "0.009099099099099",
+        "css3": "#556b2f"
+      },
+      {
+        "color": "#e1c8af",
+        "spectrum": "#e9715f",
+        "hue": "Orange",
+        "percent": "0.007837837837838",
+        "css3": "#f5deb3"
+      }
+    ],
+    "baseimageurl": "https://nrs.harvard.edu/urn-3:HUAM:CARP00896_dynmc",
+    "alttext": null,
+    "width": 508,
+    "id": 29748,
+    "lastupdate": "2022-01-06T05:06:56-0500",
+    "iiifbaseuri": "https://ids.lib.harvard.edu/ids/iiif/18211255",
+    "fileid": 4614330,
+    "height": 1024
+  }
+]
+
+export default testImages;
