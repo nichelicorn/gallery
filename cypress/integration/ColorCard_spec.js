3443b267fd95c9ea1db8ff5f0793515e010b18c8
Nichele Dunn <the7yearitch@hotmail.com>
2022-01-13 16:29:43 -0700

    🧪 Set up skeleton for ColorCard component test

---
+++ cypress/integration/ColorCard_spec.js
@@ -0,0 +1,39 @@
+// These tests describe the ColorCard component
+// Each card displays an artwork with a color overlay
+
+describe.skip("The ColorCard component", () => {
+
+  // 3. As a user, I will see artwork in that color value displayed within a rainbow / spectrum bubble.
+  // - [ ] The artwork will have a color overlay that matches the spectrum color
+  // - [ ] Hovering over an artwork will display a detail about the work (artist / maker *or* place if maker not available)
+  // - [ ] If there is an error in fetching artwork, display the color only in the bubble, and (or? what is better UX?) display an error message
+
+  it("Should render a ColorCard component", () => {
+
+  })
+
+  it("Should store a hue value, artwork URL, and Routing", () => {
+
+  })
+
+  it("Should display the associated artwork in the card, based on the image URL", () => {
+
+  })
+
+  it("Should have an overlay on top of the artwork that matches its spectral hue", () => {
+
+  })
+
+  it("Should show the artwork details when a user hovers over the artwork", () => {
+
+  })
+
+  it("Should have Routing to a unique URL based on the image id", () => {
+
+  })
+
+  it("Should show an error icon if the artwork is not available to display", () => {
+    
+  })
+
+})
