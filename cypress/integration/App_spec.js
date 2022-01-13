7f265174e80c410c0bdf77902e5fe2cb7cb53ac4
Nichele Dunn <the7yearitch@hotmail.com>
2022-01-13 16:13:39 -0700

    ♻️  Refactor App component test pseudo

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -3,33 +3,21 @@ describe.skip("The App component", () => {
 
   // 1. As a user, when I visit the app, I should see a rainbow on the dashboard
 // - [ ] When user visits url, a homepage loads
-// - [ ] home view includes header with navigation, main dashboard view with spectrum / rainbow
-// 2. As a user, I should see a rainbow / spectrum display in the main window
-// - [ ] This display should have an area for each color value of the spectrum
-// - [ ] There should be seven total color areas
-// - [ ] Eventually, there will be an artwork displayed within each color
+// - [ ] home view includes header with navigation, main dashboard view with spectrum / rainbow, and footer with social media icon links
 
-  it("Should allow a user to visit the home URL, and display the Spectrum dashboard", () => {
+  it("Should allow a user to visit the home URL", () => {
+
+  })
+  
+  it("Should render a header with navigation elements", () => {
+  
+  })
+
+  it("Should render a dashboard with a section to display artwork", () => {
 
   })
 
-  it("Should display a dashboard with rainbow that fills the dashboard", () => {
-
-  })
-
-  it("Should  have seven colors and an image in each color", () => {
-
-  })
-
-  it("Should display an error to the user if image data is not available", () => {
-
-  })
-
-  it("Should have a header with navigation elements", () => {
-
-  })
-
-  it("Should have a footer with social media links and copywrite dates", () => {
+  it("Should render a footer with social media links and copywrite dates", () => {
 
   })
 
---
+++ cypress/integration/Spectrum_spec.js
@@ -0,0 +1,21 @@
+// These tests describe the Spectrum component
+// This is the dashboard, and displays a rainbow, with artwork displayed in each color
+
+describe.skip("The Spectrum component", () => {
+
+  // 2. As a user, I should see a rainbow / spectrum display in the main window
+  // - [ ] This display should have an area for each color value of the spectrum
+  // - [ ] There should be seven total color areas
+  // - [ ] Eventually, there will be an artwork displayed within each color
+
+  it("Should  have seven colors and an image in each color", () => {
+
+  })
+
+  it("Should display an error to the user if image data is not available", () => {
+
+  })
+
+
+  
+})
